c6613c26e9d712d7a2e3284d2cb4ef1009ecf56d
Steven McNeill <smcneill.uk@gmail.com>
2018-11-26 15:39:16 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+
+var app = express();
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear()
+});
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if(err) {
+            console.log('Unable to append to server.log')
+        }
+    });
+
+    next();
+});
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs', {
+//         pageTitle: 'Maintenance Page',
+//         pageText: "We're currently updating the site"
+//     });
+// });
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/', (req, res) =>{
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        pageText: 'Welcome to this page!'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        pageText: 'this is random text'
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Error handling request',
+
+    });
+});
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+});
